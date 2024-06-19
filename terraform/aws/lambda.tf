8cb5c66634e7c5e3dda1d4f5b27639276859b96d
rclinton14 <rclinton@gmail.com>
2024-06-19 11:21:13 -0400

    removed hard-coded secrets from lambda.tf

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -42,8 +42,7 @@ resource "aws_lambda_function" "analysis_lambda" {
 
   environment {
     variables = {
-      access_key = "AKIAIOSFODNN7EXAMPLE"
-      secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+      
     }
   }
   tags = {
